674965c2057927993b9e0b545bebe9abfe71423b
Vigneshwar Venkatachalam <vigneshwarrv666@gmail.com>
2024-02-17 12:41:42 +0000

    updated todo app

--- src/App.js
+++ src/App.js
@@ -1,104 +1,105 @@
 //import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react'
+import { useState } from 'react'
 
 
 function App() {
 
   //default tasks
-  const defaultTasks = [{check:false,task:'Create Theme'},
-                      {check:false,task:'Work on WordPress'},
-                      {check:false,task:'Organize office main department'},
-                      {check:false,task:'Error solve in HTML template'}];
- 
+  const defaultTasks = [
+  { status: "active", task: 'Create Theme' },
+  { status: "active", task: 'Work on WordPress' },
+  { status: "active", task: 'Organize office main department' },
+  { status: "active", task: 'Error solve in HTML template' }];
+
   //hooks for new tasks
-  const [taskList,addTask] = useState(defaultTasks);  
-  const [typedTask,adding]=useState({});
-  
-  //conditional rendering for conditional rendering
-const [show,setShow] = useState(true);
-const [show1,setShow1] = useState(false);
-const [show2,setshow2] = useState(false);
+  const [taskList, setTaskList] = useState(defaultTasks);
+  const [typedTask, setTypedTask] = useState("");
 
- 
-//function to render the tasks
-function Display({check , task , type , id , remove}){
-  const [strike,striking] = useState(check);
-  const style = {display:(strike)?"none":"block"};
-  const style1 = {display:(strike)?"block":"none"};
+  // hook for status based conditional rendering
+  const [show, setShow] = useState("all");
 
-  const removing=(id)=>{
-      const removedList = taskList.filter((task,index)=>index!==id);
-      remove(removedList);
-      console.log(removedList);
-  }
-
-
-return(
-  <div className="tasks">
- 
-        
-   {(type==="filter") 
-     ? (check===false)
-           ?<div style={style} className="task">
-               <input type="checkbox" onClick={()=>{striking(!strike); 
-                                              for(let i of taskList){
-                                                if(i.task===task){
-                                                    i.check = true;
-                                                }};}}/>
-                <span>{task}</span>
-            </div>
-           : (check===true)
-              ?<div style={style1} className="task">
-                <input type="checkbox" defaultChecked onClick={()=>{striking(!strike); 
-                                               for(let i of taskList){
-                                                 if(i.task===task){
-                                                     i.check = false;
-                                                 }};}}/>
-                 <span><strike>{task}</strike></span>
-                 </div>
-           :""
-     :  (check)  
-              ? <div className="task"><input type="checkbox" defaultChecked onClick={()=>{striking(!strike); 
-                                               for(let i of taskList){
-                                                 if(i.task===task){
-                                                   i.check = !strike;
-                                                 }};}}/>
-                {(strike)?<span><strike>{task}</strike></span>:<span>{task}</span>}
-                <button onClick={()=>removing(id)}>Remove</button></div>
-              : <div className="task"><input type="checkbox" onClick={()=>{striking(!strike); 
-                                                 for(let i of taskList){
-                                                 if(i.task===task){
-                                                   i.check = !strike;
-                                                 }};}}/>
-               {(strike)?<span><strike>{task}</strike></span> :<span>{task}</span>}
-               <button onClick={()=>removing(id)}>Remove</button></div>} 
-  </div>);}
- 
   return (
     <div className="App">
       <h1>Here's your TODO List</h1>
       <div className="addTask-grid">
-      <div className="addTask">
-       <input placeholder="Enter the TASK" onChange={(event)=>adding({check:false, task:(event.target.value)})}/>
-       <button className="button" onClick={()=>{(typedTask.task!==undefined)?addTask([...taskList,typedTask]):alert("Enter task to add");}}>Add</button>
-       </div>
+        <div className="addTask">
+          <input placeholder="Enter the TASK" value={typedTask} onChange={(event) => setTypedTask(event.currentTarget.value)} />
+          <button className="button" onClick={() => { 
+            if(typedTask !== "") { 
+              setTaskList([...taskList, {status: "active", task: typedTask}]);
+              setTypedTask("");
+            } else {
+              alert("Enter task to add"); 
+            }
+          }}>Add</button>
+        </div>
       </div>
-       <div className="status-and-tasks-grid">
-         <div className="taskStatus">
-       <button onClick={()=>{setShow(true);setShow1(false);setshow2(false)}}>All</button>
-       <button onClick={()=>{setShow(false);setShow1(true);setshow2(false)}}>Active</button>
-       <button onClick={()=>{setShow(false);setShow1(false);setshow2(true)}}>Completed</button>
-         </div>
-         {(show) ? (taskList.map(({check,task},index)=><Display key={index} id={index} check={check} task={task} remove={addTask} type="All" />)) : ""}
-
-         {(show1) ? (taskList.filter((task)=>{return(task.check===false)}).map(({task,check},index)=><Display key={index} check={check} task={task} type="filter" />)) :""}
-         
-         {(show2) ? (taskList.filter((task)=>{return(task.check===true)}).map(({task,check},index)=><Display key={index} check={check} task={task} type="filter"/>)) :""}
-       </div>
+      <div className="status-and-tasks-grid">
+        <div className="taskStatus">
+          <button onClick={() => { setShow("all") }}>All</button>
+          <button onClick={() => { setShow("active") }}>Active</button>
+          <button onClick={() => { setShow("completed") }}>Completed</button>
+        </div>
+        <div className="tasks">
+          {taskList.map(({status, task}, index)=>{
+            if(show === "all" || show === status) {
+              return (
+                <Task id={index} status={status} task={task} updateTaskList={setTaskList} />
+              )
+            } else {
+              return <></>
+            }
+          })}
+        </div>
+      </div>
     </div>
   );
 }
+
+//function to render the tasks
+function Task({ status, task, id, updateTaskList }) {
+
+  const removeTask = (id) => {
+    updateTaskList((taskList)=> taskList.filter((task, index) => index !== id));
+  };
+  
+  const updateTaskStatus = (id, updatedStatus) => {
+    updateTaskList((taskList)=> taskList.map((task, index) => {
+        if(index === id) {
+          task.status = updatedStatus;
+        }
+        return task;
+      })
+    );
+  };
+
+  console.log("rendering");
+
+  return (
+    <>
+      { <div className='task'>
+          <input type="checkbox" checked={status === "completed"} onChange={(e) => {
+            if (e.currentTarget.checked) {
+              updateTaskStatus(id, "completed");
+            } else {
+              updateTaskStatus(id, "active");
+            }
+          }} />
+
+          {(status === "completed") 
+            ? <span><strike>{task}</strike></span> 
+            : <span>{task}</span>}
+
+          <button onClick={() => removeTask(id)}>Remove</button>
+
+        </div>
+      }
+    </>
+  );
+
+}
+
 
 
 
